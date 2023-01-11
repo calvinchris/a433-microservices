9bfd816cb3a4891683e72625d7fd130aec6eff49
calvinchris <calvinchris15@gmail.com>
2023-01-12 00:12:46 +0800

    Perubah Script Build

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Perintah Build Docker Image dengan Dockerfile
-docker build -t calvinchris-docker/karsajobs-ui:latest .
+docker build -t ghcr.io/calvinchris/karsajobs-ui:latest .
 
 #Perintah Login ke Github Container Registry
 echo $GHCR_TOKEN | docker login ghcr.io -u calvinchris --password-stdin
 
 #Perintah mengupload docker image ke Github Container Repository
-docker push calvinchris-docker/karsajobs-ui:latest
+docker push ghcr.io/calvinchris/karsajobs-ui:latest
